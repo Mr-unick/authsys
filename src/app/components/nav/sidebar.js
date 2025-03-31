68ac8b49c32cdfb83b4bf04c09261e3f8f6d34e2
madhav-csinc <madhav.jha@csinc.in>
2025-04-01 03:13:51 +0530

    Added Permissions

--- src/app/components/nav/sidebar.js
+++ src/app/components/nav/sidebar.js
@@ -67,7 +67,7 @@ export default function SideBar({ setOpen }) {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:3000/api/getSidebarProps`)
+    axios.get(`https://authsys-client.vercel.app/api/getSidebarProps`)
       .then((res) => res.data)
       .then((res) => {
         setSideBarData(res.data);
