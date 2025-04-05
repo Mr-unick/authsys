36c03228350fae0076f3392c96ea12bbafba6325
Nikhil <lendenikhil9121@gmail.com>
2025-04-05 16:27:54 +0530

    Board Leads Chnages

--- src/pages/api/leaddetails/getleaddetails.ts
+++ src/pages/api/leaddetails/getleaddetails.ts
@@ -103,7 +103,6 @@ export default async function handler(req, res) {
 
         return res.json(response);
 
-
     } catch (error) {
         return res.status(500).json({
             message: "Internal server error",
