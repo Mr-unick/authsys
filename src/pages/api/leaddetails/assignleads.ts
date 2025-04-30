3b894078866ae1784e37845160679c8a88f8d0a8
Nikhil Lende <110834015+Mr-unick@users.noreply.github.com>
2025-04-30 15:19:46 +0530

    Update assignleads.ts

--- src/pages/api/leaddetails/assignleads.ts
+++ src/pages/api/leaddetails/assignleads.ts
@@ -64,12 +64,14 @@ export default async function assignLeads(req, res) {
 
                    // lead.users.push(user);
 
-                    const users = await AppDataSource.getRepository(Users).findBy({id: userId});
-                    lead.users = [...lead.users, ...users];
-                    await AppDataSource.getRepository(Leads).save(lead);
+                    // const users = await AppDataSource.getRepository(Users).findBy({id: userId});
+                    // lead.users = [...lead.users, ...users];
+                    // await AppDataSource.getRepository(Leads).save(lead);
                 }
             }
-
+  const users = await AppDataSource.getRepository(Users).findBy({id: In(salespersons)});
+                    lead.users = [...lead.users, ...users];
+                    await AppDataSource.getRepository(Leads).save(lead);
           //  await AppDataSource.getRepository(Leads).save(lead);
         }
 
