15ae266f18a04c7f5614ea43c9908bb715ee4c6a
Nikhil <lendenikhil9121@gmail.com>
2025-07-16 15:42:38 +0530

    change lead stage

--- src/app/entity/StageChangeHistory.ts
+++ src/app/entity/StageChangeHistory.ts
@@ -5,6 +5,7 @@ import { Users } from './Users';
 import { type } from 'node:os';
 import { Columns } from 'lucide-react';
 
+
 @Entity('status_change_history')
 export class StageChangeHistory {
   @PrimaryGeneratedColumn()
@@ -14,17 +15,17 @@ export class StageChangeHistory {
   changed_at: Date;
 
   @Column()
-  reason: string; 
+  reason: string;
 
-  @ManyToOne(type => LeadStages, (stage) => stage.stageChangeHistory)
-  @JoinColumn({name:'stageId'})
-  stage:typeof LeadStages;
+  @ManyToOne(() => require('./LeadStages').LeadStages, (stage) => stage.stageChangeHistory)
+  @JoinColumn({ name: 'stageId' })
+  stage: any;
 
-  @ManyToOne(type => Leads, (lead) => lead.history)
-  @JoinColumn({ name: 'lead_id' }) 
-  lead:typeof Leads;
+  @ManyToOne(() => require('./Leads').Leads, (lead) => lead.history)
+  @JoinColumn({ name: 'lead_id' })
+  lead: any;
 
-  @ManyToOne(() => Users, user => user.history)
+  @ManyToOne(() => require('./Users').Users, (user) => user.history)
   @JoinColumn({ name: 'changed_by' })
-  changed_by:typeof Users;
+  changed_by: any;
 }
